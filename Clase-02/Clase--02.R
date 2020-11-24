f80edd0c4a817fefe040dba3f725b6a65cff4605
Naty Esquenazi <naty.esquenazi@gmail.com>
2020-11-23 23:47:38 -0300

    Añadi cambios en el segundo script, falta el ppt

--- Clase-02/Clase--02.R
+++ Clase-02/Clase--02.R
@@ -1,7 +1,7 @@
 
 ####################### CLASE 02 ################################ ---------------------
 
-###################### Cargar datos ############################ ----------------------
+###################### CARGAR DATOS ############################ ----------------------
 
 ## ¿Cómo podemos cargar la base de datos en el formato .csv? Como puedes sospechar, empezaremos con la base de datos más simple de cargar. Gracias a la función read_csv() en readr, sólo tendremos que escribir la ruta del archivo dentro de nuestro proyecto:
 
@@ -11,7 +11,6 @@ df <- read_csv("delitos_2019.csv")
 df <- read_csv("data/desiguales.csv",
                col_types = cols_only_chr(c("age", # Columnas que queremos seleccionar 
                                            "p2")))
-
 ## Excel 
 
 install.packages("readxl")
@@ -25,6 +24,39 @@ library(haven)
 df <- read_spss("Clase-02/delitos_2019.sav")
 df <- read_stata("Clase-02/delitos_2019.dta")
 
+####################### FUNCIONES EXPLORATORIAS ############### -------------------
+install.packages("tidyverse")
+library(tidyverse)
+install.packages("funmodeling")
+library(funmodeling)
+
+glimpse(df)
+str(df)
+head(df) # Primeros 5 registros
+tail(df) # Ultimos 5 registros 
+names(df) # Nombre columnas 
+View(df)
+df_status(df)
+
+
 ####################### AYUDA ################################ ---------------------
+
+## R Studio 
+
 help(sqrt) 
 ?sqrt
+
+## R pubs 
+
+## Stack Overflow 
+
+
+
+
+
+
+
+
+
+
+
